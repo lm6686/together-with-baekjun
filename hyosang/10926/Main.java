8e8b336934e1dafddbb5caaf25ded12ad67b8862
HYOSANG <lm668669@gmail.com>
2025-07-22 20:54:52 +0900

    fix: 10926 변수명 변경

--- hyosang/10926/Main.java
+++ hyosang/10926/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Q10926{
+class Main{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         String s = scanner.nextLine();
